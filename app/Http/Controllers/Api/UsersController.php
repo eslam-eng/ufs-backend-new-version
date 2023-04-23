fbbd611bfedb43b4d92a5d950f450b97c8b0db01
eslam <elbadry693@gmail.com>
2023-04-23 23:52:53 +0200

    add login and fcm from request

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,13 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Exceptions\NotFoundException;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\StoreFcmTokenRequest;
-use App\Services\AuthService;
-use Exception;
-use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
