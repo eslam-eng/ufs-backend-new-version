186b5aaa6f9fa05a2f26dfd880f13425e352be9d
eslam <elbadry693@gmail.com>
2023-04-23 23:51:54 +0200

    add login and fcm from request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,19 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Exceptions\NotFoundException;
-use App\Exceptions\UserNotFoundException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\StoreFcmTokenRequest;
-use App\Http\Requests\UpdateUserRequest;
-use App\Http\Requests\UserUpdateLogoRequest;
-use App\Http\Resources\AuthUserResource;
-use App\Models\User;
 use App\Services\AuthService;
 use Exception;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -25,68 +18,13 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
-
         try {
-            $user = $this->authService->loginWithUsernameOrPhone(identifier: $request->identifier, password: $request->password,type:$type);
-            $this->authService->setUserFcmToken($user,$request->fcm_token);
-            return new AuthUserResource($user);
+            $user = $this->authService->loginWithEmailOrPhone(identifier: $request->identifier, password: $request->password);
+            if (isset($request->fcm_token))
+                $this->authService->setUserFcmToken($user,$request->fcm_token);
+            return $user;
         } catch (Exception|NotFoundException $e) {
             return apiResponse($e->getMessage(), 'Unauthorized', $e->getCode());
-        }
-
-    }
-
-    public function register(RegisterRequest $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
-    {
-        $data = $request->validated();
-        $data = array_merge($data, ['type' => User::CUSTOMERTYPE, 'last_login_at' => now()]);
-        $data['name'] = [
-            'en' => $data['name'],
-            'ar' => $data['name'],
-        ];
-        $data['password'] = bcrypt($data['password']);
-        $result = $this->authService->register(data: $data);
-        if ($result)
-            return apiResponse( trans('lang.success'));
-        return apiResponse(message: __('lang.error_message'), code: 422);
-    }
-
-
-    public function authUser(): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
-    {
-        try {
-            $user = Auth::user()->load(['location','attachments']);
-            if ($user->type == User::CENTERADMIN)
-                $user->load(['center.attachments']);
-            return apiResponse(data: new AuthUserResource($user));
-        } catch (\Exception $exception) {
-            logger('auth user exception');
-            return apiResponse(message: $exception->getMessage(), code: 422);
-        }
-    }
-
-    public function update(UpdateUserRequest $request, $user)//: \Illuminate\Http\RedirectResponse
-    {
-        try {
-            DB::beginTransaction();
-            $user = User::find($user);
-            $user = $this->authService->update($user, $request->validated());
-            DB::commit();
-            return apiResponse(data: new AuthUserResource($user), message: trans('lang.success_operation'));
-        } catch (\Exception $exception) {
-            return apiResponse(message: $exception->getMessage(), code: 422);
-        }
-    }
-
-    public function updateProfileImage(UserUpdateLogoRequest $request)
-    {
-        try{
-            $user = $this->authService->updateLogo(data: $request->validated());
-            if(!$user)
-                return apiResponse(message: trans('lang.some_thing_went_rong'), code: 422);
-            return apiResponse(data: new AuthUserResource($user), message: trans('lang.success_operation'));
-        }catch(Exception $e){
-            return apiResponse(message: $e->getMessage(), code: 422);
         }
     }
 
