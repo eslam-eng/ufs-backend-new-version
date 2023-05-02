b4311e0fb2b281d96b44fa6cdc1577d8b698891d
eslam <elbadry693@gmail.com>
2023-05-03 02:02:24 +0300

    fix branches edits

--- database/seeders/BranchesTableSeeder.php
+++ database/seeders/BranchesTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Branch;
 use App\Models\Company;
 use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class BranchesTableSeeder extends Seeder
 {
@@ -15,10 +16,22 @@ class BranchesTableSeeder extends Seeder
     public function run(): void
     {
         $company_id = Company::first()->id ;
-        Branch::create([
+      $branch =   Branch::create([
             'name' => 'branch 1 test',
             'company_id' => $company_id,
             'phone'=>'01113622098'
         ]);
+
+        $city_id = Location::withDepth()->having('depth', '=', 1)->first()->id; // المحافظات
+        $area_id = Location::withDepth()->having('depth', '=', 2)->first()->id; //
+      $branch->storeAddress([
+          'city_id' => $city_id,
+          'area_id' => $area_id,
+          'address' => Str::random(10),
+          'lat' => 12121.1212,
+          'lng' =>12121.1212,
+          'postal_code' => 12345,
+          'is_default' => true
+      ]);
     }
 }
