7254b11db5aabb5362f9b0ade64c1637ef7363f3
eslam <elbadry693@gmail.com>
2023-05-02 23:44:30 +0300

    fix addresses crud

--- app/Traits/HasAddresses.php
+++ app/Traits/HasAddresses.php
@@ -17,11 +17,7 @@ trait HasAddresses
 
     public function storeAddress(array $data = [])
     {
-        $address = $this->addresses()->create($data);
-        $is_default = isset($data['is_default']) && $data['is_default'] != 0 ? 1:0; 
-        if($address && $is_default)
-                $this->setDefaultAddress(address: $address);
-        return $address;
+        return $this->addresses()->create($data);
     }
 
     public function deleteAddresses(): int
@@ -29,29 +25,5 @@ trait HasAddresses
         return $this->addresses()->delete();
     }
 
-    public function deleteCustomAddress(int $id)
-    {
-        $this->addresses->where('id', $id)->first()->delete();
-    }
 
-    public function updateAddress(array $data = [], int $id)
-    {
-        $address = $this->addresses->where('id', $id)->first();
-        $address->update(Arr::except($data, 'is_default'));
-        $is_default = isset($data['is_default']) && $data['is_default'] != 0 ? 1:0; 
-        if($address && $is_default)
-                $this->setDefaultAddress(address: $address);
-        
-    }
-
-    public function setDefaultAddress(Address $address)
-    {
-        $defaultAddress = $this->addresses->where('is_default', ActivationStatus::ACTIVE->value)->first();
-        $defaultAddress->is_default = ActivationStatus::INACTIVE;
-        $defaultAddress->save();
-        $address->is_default = ActivationStatus::ACTIVE;
-        $address->save();
-    }
-
-    
 }
