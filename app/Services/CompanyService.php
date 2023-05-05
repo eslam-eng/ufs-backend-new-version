beb3e2facb085fa276424325f5a5cd7f9ff456fa
eslam <elbadry693@gmail.com>
2023-05-06 01:15:34 +0300

    edits in company

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -2,36 +2,90 @@
 
 namespace App\Services;
 
+use App\DTO\Branch\BranchDTO;
+use App\DTO\Company\CompanyDTO;
+use App\Exceptions\NotFoundException;
 use App\Models\Company;
-use App\QueryFilters\LocationsFilter;
+use App\QueryFilters\CompaniesFilter;
+use App\Services\BranchService;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class CompanyService extends BaseService
 {
 
-    public function __construct(public Company $model)
+    public function __construct(public Company $model, private BranchService $branchService)
     {
     }
 
-    public function queryGet(array $filter = [],array $withRelations = [])
+    public function queryGet(array $filter = [],array $withRelations = []): builder
     {
         $result = $this->model->query()->with($withRelations);
-        return $result->filter(new LocationsFilter($filter));
+        return $result->filter(new CompaniesFilter($filter));
     }
 
 
-    public function getAll(array $filters = [])
+    public function listing(array $filters = [], $withRelations  = [], $perPage = 10): \Illuminate\Contracts\Pagination\CursorPaginator
     {
-        return $this->queryGet($filters)->get();
+        return $this->queryGet($filters)->cursorPaginate($perPage);
     }
 
-    public function getLocationAncestors($id)
+    public function getCompaniesForSelectDropDown(array $filters = []): \Illuminate\Database\Eloquent\Collection|array
     {
-        return $this->model->defaultOrder()->ancestorsAndSelf($id);
+        return $this->queryGet(filter: $filters)->select(['id','name'])->get();
     }
 
-    public function getLocationDescendants($location_id)
+    /**
+     * create new receiver
+     * @param array $data
+     * @return bool
+     */
+    public function store(CompanyDTO $companyDTO): bool
     {
-        return $this->model->defaultOrder()->descendantsOf($location_id);
+        $company = $this->model->create($companyDTO->companyData());
+        $company->storeAddress($companyDTO->addressData());
+        foreach($companyDTO->branchesData() as $branch)
+        {
+            $branch['company_id'] = $company->id;
+            $this->branchService->store(BranchDTO::fromArray(data: $branch));
+        }
+
+        foreach($companyDTO->departmentsData() as $department)
+            $company->departments()->create($department);
+
+        return true;
+    }
+
+    /**
+     * update existing company
+     * @param array $data
+     * @param int $id
+     * @return bool
+     * @throws NotFoundException
+     */
+    public function update(int $id, CompanyDTO $companyDTO): bool
+    {
+        $company = Company::find($id);
+        if (!$company)
+            throw new NotFoundException(trans('lang.not_found'));
+        $company->update($companyDTO->companyData());
+        return true;
+    }
+
+    /**
+     * delete existing company
+     * @param int $id
+     * @return bool
+     * @throws NotFoundException
+     */
+    public function destroy(int $id): bool
+    {
+        $company = Company::find($id);
+        if (!$company)
+            throw new NotFoundException(trans('lang.not_found'));
+        $company->delete();
+        $company->deleteAddresses();
+        return true;
     }
 
 }
